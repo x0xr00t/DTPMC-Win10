0a5970723cb1110c3af8192dfa3d2caca40036eb
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-03-08 17:27:18 +0100

    Update dtpmc.ps1

--- dtpmc.ps1
+++ dtpmc.ps1
@@ -7,25 +7,25 @@
 
 
 Write-Host "000000000000slo0ppyr00t000000000000"
-Write-Host "0 Sl0ppyR00tNullify Lua Settings  0"
+Write-Host "0 Sl0ppyR00t Disable TPM Settings 0"
 Write-Host "000000000000slo0ppyr00t000000000000"
 Write-Host ""
 Write-Host ""
-Write-Host " {Sl0ppyR00t} Getting LUA Settings...."
+Write-Host " {Sl0ppyR00t} Chaning Dir... "
+Set-Location -Path HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
+Write-Host "" 
+Write-Host ""
+Write-Host " {Sl0ppyR00t} Making TPM Settings File..."
+New-Item -Path 'HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\' -Name  'AllowUpgradesWithUnsupportedTPMOrCPU' -Value '1' -PropertyType 'DWORD' -Force
+Write-Host ""
+Write-Host ""
+sleep 4
+Write-Host " {Sl0ppyR00t} Getting TPM Settings...."
 sleep 4
 Write-Host "" 
 Write-Host "{Sl0ppyR00t} Initializing..."
 Write-Host "" 
-Get-ItemProperty HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system
+Get-ItemProperty HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\AllowUpgradesWithUnsupportedTPMOrCPU
 Write-Host ""
 Write-Host ""
-sleep 4
-Write-Host "000000000000slo0ppyr00t000000000000"
-Write-Host "0 Sl0ppyR00t Nullify EnableLUA    0"
-Write-Host "000000000000slo0ppyr00t000000000000"
-sleep 5
-$val = Get-ItemProperty -Path hklm:software\microsoft\windows\currentversion\policies\system -Name "EnableLUA"
-if($val.EnableLUA -ne 0)
-{
- set-itemproperty -Path hklm:software\microsoft\windows\currentversion\policies\system -Name "EnableLUA" -value 0 
-}
+Write-Host " {Sl0ppyr00t} Place file is done, reboot ur system, and if needed enter the bios setting disable the tpm"
