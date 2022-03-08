66f97ba00af257cb51f97571d0445fe2a385b236
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-03-08 17:36:49 +0100

    Update dtpmc.ps1

--- dtpmc.ps1
+++ dtpmc.ps1
@@ -11,11 +11,11 @@ Write-Host "000000000000slo0ppyr00t000000000000"
 Write-Host ""
 Write-Host ""
 Write-Host " {Sl0ppyR00t} Chaning Dir... "
-Set-Location -Path HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
+Set-Location -Path Registry::HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
 Write-Host "" 
 Write-Host ""
 Write-Host " {Sl0ppyR00t} Making TPM Settings File..."
-New-Item -Path 'HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\' -Name  'AllowUpgradesWithUnsupportedTPMOrCPU' -Value '1' -PropertyType 'DWORD' -Force
+New-Item -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\' -Name  'AllowUpgradesWithUnsupportedTPMOrCPU' -Value '1'  -Force
 Write-Host ""
 Write-Host ""
 sleep 4
@@ -24,7 +24,7 @@ sleep 4
 Write-Host "" 
 Write-Host "{Sl0ppyR00t} Initializing..."
 Write-Host "" 
-Get-ItemProperty HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\AllowUpgradesWithUnsupportedTPMOrCPU
+Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup\AllowUpgradesWithUnsupportedTPMOrCPU
 Write-Host ""
 Write-Host ""
 Write-Host " {Sl0ppyr00t} Place file is done, reboot ur system, and if needed enter the bios setting disable the tpm"
